81f41a268f8bc73ada7e5705edc15b8e6d215e0a
davidan52 <69400846+davidan52@users.noreply.github.com>
2021-11-22 10:29:12 +0200

    Create RectangleB.java

---
+++ src/RectangleB.java
@@ -0,0 +1,18 @@
+public class RectangleB {
+
+
+
+    public RectangleB(int w, int h){
+
+    }
+    public RectangleB(Point p, int w, int h){
+
+    }
+    public RectangleB(Point sw, Point ne){
+
+    }
+    public RectangleB(RectangleB r){
+
+    }
+
+}
